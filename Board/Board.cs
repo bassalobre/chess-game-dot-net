5fcca43de416a7c864cffc0188738633d6268192
William Bassalobre <williambassalobre@gmail.com>
2020-01-19 16:56:27 -0300

    Starting create the Board.

---
+++ Board/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Board
+{
+    class Board
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private Piece[,] Pieces { get; set; }
+
+        public Board(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Pieces = new Piece[Rows, Columns];
+        }
+    }
+}
