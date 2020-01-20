98cb32340608fdf04cbf8c1732cc89de361f8d8f
William Bassalobre <williambassalobre@gmail.com>
2020-01-19 21:48:24 -0300

    Created ChessPosition class.

---
+++ Game/ChessPosition.cs
@@ -0,0 +1,32 @@
+﻿using ChessGame.Board;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Game
+{
+    class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return new StringBuilder()
+                .Append(Column)
+                .Append(Row)
+                .ToString();
+        }
+    }
+}
